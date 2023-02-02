41f926001378777a4a1f0549ceb6e590377d528c
Koichi Kiyokawa <koichi20110068@gmail.com>
2023-02-02 23:18:31 +0900

    fix: release script

--- scripts/release.ts
+++ scripts/release.ts
@@ -1,10 +1,7 @@
-import bumpp from "bumpp";
 import { execSync } from "child_process";
-import { name, version } from "../package.json";
 
 (async () => {
-  await bumpp();
-  execSync("pnpm run build");
+  const { name, version } = await import("../package.json");
   // enable to match zip folder name with unzip folder name
   execSync(`cp -r dist ${name} && zip -r ${name}.zip ${name}/ && rm -rf ${name}`);
   execSync(`gh release create v${version} ${name}.zip --title '' --notes ''`, { stdio: "inherit" });
