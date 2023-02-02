aa2d7ddbc32670233436c7853463c2c12b6dcb4e
Koichi Kiyokawa <koichi20110068@gmail.com>
2023-02-02 22:02:12 +0900

    chore: cleanup

---
+++ src/manifest.ts
@@ -0,0 +1,22 @@
+import fs from "node:fs";
+import path from "node:path";
+import { name, version } from "../package.json";
+
+console.log(__dirname);
+
+const manifest: chrome.runtime.ManifestV3 = {
+  manifest_version: 3,
+  name,
+  version,
+  content_scripts: [
+    {
+      matches: ["https://github.com/*"],
+      js: ["content.js"],
+    },
+  ],
+};
+
+fs.writeFileSync(
+  path.resolve(__dirname, "../dist/manifest.json"),
+  JSON.stringify(manifest, null, 2)
+);
